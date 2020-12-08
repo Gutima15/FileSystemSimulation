eb8998b487fb4136e7e4982a1b754d8e4a6fdf51
Jorge Gutiérrez <jorgeedugutima15@gmail.com>
2020-12-08 11:48:21 -0600

    Bazlur Rahman (Rokon12) fileSystem Base
    This is an update the base code from https://gist.github.com/rokon12/d3c83562c785de6d1a483a5585205b92 which we'll be editing and adding more functions

--- FileSystemSimulation/src/filesystemsimulation/FileSystemSimulation.java
+++ FileSystemSimulation/src/filesystemsimulation/FileSystemSimulation.java
@@ -6,8 +6,10 @@
 package filesystemsimulation;
 
 /**
- *
- * @author adria
+ * @authors Jorge Gutiérrez, Luis Adrián Badilla
+ * @Reference Source Bazlur Rahman Rokon (31/3/17)
+ * @Original at: https://gist.github.com/rokon12/d3c83562c785de6d1a483a5585205b92
+ * @Updated 8/12/2020
  */
 public class FileSystemSimulation {
 
@@ -15,7 +17,33 @@ public class FileSystemSimulation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FileSystemDirectory root;
+        root = new FileSystemDirectory("root");
+        FileSystemFile file = new FileSystemFile("profile.jpg");
+        root.add(file);
+
+        FileSystemDirectory movie = new FileSystemDirectory("movie");
+        root.add(movie);
+
+        FileSystemDirectory englishMovie = new FileSystemDirectory("english");
+
+        englishMovie.add(new FileSystemDirectory("IronFist.mp4"));
+        englishMovie.add(new FileSystemDirectory("The Shawshank Redemption.mp4"));
+        englishMovie.add(new FileSystemDirectory("ZotaPia.mp4"));
+        FileSystemFile despicableMe = new FileSystemFile("DespicableMe.mp4");
+        englishMovie.add(despicableMe);
+        movie.add(englishMovie);
+
+        FileSystemDirectory banglaMovie = new FileSystemDirectory("Bangla");
+        banglaMovie.add(new FileSystemFile("The Clay Bird.mp4"));
+        banglaMovie.add(new FileSystemFile("Jibon Thekey Neya.mp4"));
+
+        movie.add(banglaMovie);
+
+        root.printTree();
+
+        System.out.println("name: " + movie.getName());
+        System.out.println("Created: " + movie.getCreated());
     }
     
 }
